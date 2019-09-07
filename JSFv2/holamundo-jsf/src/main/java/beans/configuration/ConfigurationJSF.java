b0ba7610ce62268cbddcb9d2adb5e3acf1643a48
Diego Scifo <diegoscifo@gmail.com>
2019-09-07 19:02:11 -0300

    update .gitignore

---
+++ JSFv2/holamundo-jsf/src/main/java/beans/configuration/ConfigurationJSF.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans.configuration;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+
+/**
+ *
+ * @author Almis
+ */
+@FacesConfig(version = JSF_2_3)
+@ApplicationScoped
+public class ConfigurationJSF {
+    
+}
