3501e1a515920fe535f5674cd39d68d9b8a0eb05
Diego Scifo <diegoscifo@gmail.com>
2019-09-14 21:40:14 -0300

    Curso de JSF

---
+++ JSFv2/holamundo-jsf/src/main/java/beans/model/Candidato.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans.model;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Almis
+ */
+@Named
+@RequestScoped
+public class Candidato {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
