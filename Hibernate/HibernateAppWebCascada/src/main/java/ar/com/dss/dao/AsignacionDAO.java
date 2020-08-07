db8b6b62e34526e03b8b5140eed97ed69a77e899
Diego Scifo <diegoscifo@gmail.com>
2020-08-07 06:32:46 -0300

    Cascade Persistence

---
+++ Hibernate/HibernateAppWebCascada/src/main/java/ar/com/dss/dao/AsignacionDAO.java
@@ -0,0 +1,67 @@
+package ar.com.dss.dao;
+
+import java.util.List;
+import javax.persistence.Query;
+import static ar.com.dss.dao.GenericDAO.em;
+import ar.com.dss.domain.Asignacion;
+
+public class AsignacionDAO extends GenericDAO {
+
+    public List<Asignacion> listar() {
+        String consulta = "SELECT a FROM Asignacion a";
+        em = getEntityManager();
+        Query query = em.createQuery(consulta);
+        return query.getResultList();
+    }
+
+    public void insertar(Asignacion asignacion) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(asignacion);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void actualizar(Asignacion asignacion) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.merge(asignacion);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void eliminar(Asignacion asignacion) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.remove(em.merge(asignacion));
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+    
+    public Object buscarPorId(Asignacion asignacion){
+        em = getEntityManager();
+        return em.find(Asignacion.class, asignacion.getIdAsignacion());
+    }
+
+}
